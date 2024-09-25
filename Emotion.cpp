6b4b72e8c226c2598b47c85229d69289ed84d769
aleg <aleg302@yahoo.com>
2024-09-25 19:13:41 +0300

    commit

--- Emotion.cpp
+++ Emotion.cpp
@@ -7,9 +7,12 @@ using namespace std;
 void Emotion::brutforce()
 {
 //setlocale(LC_ALL, "en_US.UTF-8");
-wchar_t codepoint;
-for( wchar_t i = 0; i <= 0x00ff; i++ ) {
-    std::wcout << "i" << std::endl;
+//wchar_t codepoint;
+for( wchar_t i = 0; i <= 0xffff; i++ ) {
+    int val = i; // 0x1234
+    std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t> converter;
+    std::string u8str = converter.to_bytes(val);
+    cout << u8str;
 }
  
 
